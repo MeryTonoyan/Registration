35227be7d01e07eed11b02af42fdf618ee15c286
MeryTonoyan <anahit.yan9898@mail.ru>
2026-02-04 22:45:46 +0400

    I make a new registration site

--- frontend/script.js
+++ frontend/script.js
@@ -1,52 +1,89 @@
-const $ = (s) => document.querySelector(s);
+const $ = s => document.querySelector(s)
 
-fetch('http://localhost:3000/')
+fetch('http://localhost:3001/profile', { credentials: 'include' })
     .then(res => res.json())
     .then(data => {
-        console.table(data);
-    });
+        console.table(data)
+        if (data.id) {
+            $('#guestArea').classList.add('d-none')
+            $('#appArea').classList.remove('d-none')
+            $('#profileName').value = data.name
+            $('#profileEmail').value = data.email
+        }
+    })
 
-$('#registerForm').onsubmit = function(e) {
-    e.preventDefault();
+$('#registerForm').onsubmit = (e) => {
+    e.preventDefault()
 
-    let userObj = {
+    let data = {
         name: $('#regName').value,
         email: $('#regEmail').value,
         password: $('#regPassword').value,
-        role: $('#regIsAdmin').checked ? 'admin' : 'user'
-    };
+        isAdmin: $('#regIsAdmin').checked
+    }
 
-    fetch('http://localhost:3000/add', {
+    fetch('http://localhost:3001/register', {
         method: 'POST',
-        body: JSON.stringify({ value: userObj }),
-        headers: { 'Content-Type': 'application/json' }
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include'
     })
         .then(res => res.json())
-        .then(res => {
-            console.log("User added:", res);
-            alert("Success!");
-        });
-};
-function updateUI() {
-    fetch('http://localhost:3000/profile')
+        .then(data => {
+            console.log(data)
+            location.reload()
+        })
+}
+
+$('#loginForm').onsubmit = (e) => {
+    e.preventDefault()
+    const login = $('#loginEmail').value
+    const password = $('#loginPassword').value
+
+    fetch(`http://localhost:3001/login?login=${login}&password=${password}`, {
+        credentials: 'include'
+    })
         .then(res => res.json())
         .then(data => {
-            if (data.message !== 'not logged in') {
-                $('#guestArea').classList.add('d-none');
-                $('#appArea').classList.remove('d-none');
-                $('#profileName').value = data.name || '';
-                $('#profileEmail').value = data.email || '';
-                $('#profileRole').value = data.role || '';
+            if (data.id) {
+                location.reload()
+            } else {
+                alert(data.message)
             }
+        })
+}
+
+$('#btnLogout').onclick = () => {
+    fetch('http://localhost:3001/logout', { credentials: 'include' })
+        .then(() => location.reload())
+}
+$('#adminTabBtn').onclick = () => {
+    fetch('http://localhost:3001/admin',
+        { credentials: 'include' })
+        .then(res => res.json())
+        .then(data => {
+            if (data.message === 'not admin') {
+                alert("not admin");
+                return;
+            }
+
+            $('#tabProfile').classList.add('d-none');
+            $('#tabAdmin').classList.remove('d-none');
+
+            const tbody = $('#usersTbody');
+            tbody.innerHTML = '';
+
+            data.forEach((user, index) => {
+                tbody.innerHTML += `
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>${user.role}</td>
+                    </tr>
+                `;
+            });
+
+            console.log("cucaky tarmacvec");
         });
 }
-$('#btnLogout').onclick = () => {
-    fetch('http://localhost:3000/logout')
-        .then(res => res.json())
-        .then(res => {
-            console.log(res.body);
-            location.reload();
-        });
-};
-
-updateUI();
